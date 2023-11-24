55c1e674a395e78b30ba6a37282af98cdc0faeca
storialp <96848889+storialp@users.noreply.github.com>
2023-11-24 18:11:44 +0100

    Fixed mobile stylying and changed description

--- src/components/UploadDesign.tsx
+++ src/components/UploadDesign.tsx
@@ -2,7 +2,7 @@ import { PhotoIcon } from "@heroicons/react/24/outline";
 
 export default function UploadDesign() {
   return (
-    <div className="mx-auto w-1/3">
+    <div className="mx-auto w-3/4 sm:w-1/3 ">
       <button
         type="button"
         className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
